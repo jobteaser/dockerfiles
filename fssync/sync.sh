0376ee980f9d159bd60eb7fb4b5403583ff0eed5
Ludovic Vielle <ludovic.vielle@gmail.com>
2018-05-24 17:50:26 +0200

    Create fssync image for filesystem synchronization

---
+++ fssync/sync.sh
@@ -0,0 +1,82 @@
+#!/usr/bin/env bash
+set -o errexit
+set -o nounset
+set -o pipefail
+
+out() { printf '%b\n' "$@"; }
+die() { out "$@"; exit 1; } >&2
+
+usage() {
+  cat << EOF
+Synchronize the content of the source directory to the target one.
+
+Usage: ./sync.sh [SOURCE] TARGET
+
+Options:
+
+  -h  Help about the command
+
+Notes:
+  - Without source directory, it synchronizes current directory content.
+  - If the target directory does not exist, it is created with \`mkdir -p\` command.
+EOF
+}
+
+parse_params() {
+  while getopts ":hs:t:" opt; do
+    case $opt in
+      h)
+        usage
+        exit 0
+        ;;
+      \?)
+        die "Invalid option: -${OPTARG}"
+        ;;
+      :)
+        die "Option -${OPTARG} requires an argument."
+        ;;
+    esac
+  done
+  shift "$(($OPTIND -1))"
+}
+
+
+main() {
+  parse_params "$@"
+
+  TARGET_DIR="${2:-}"
+  SOURCE_DIR="$1"
+  if [[ -z "${TARGET_DIR}" ]];
+  then
+    TARGET_DIR="$1"
+    SOURCE_DIR="$(pwd)"
+  fi
+
+  [[ -z "${TARGET_DIR}" ]] && die "$(usage)"
+  [[ ! -d "${TARGET_DIR}" ]] && mkdir -p "${TARGET_DIR}"
+
+  [[ ! -d "${SOURCE_DIR}" ]] && die "Source directory does not exist"
+
+  pushd "${SOURCE_DIR}"
+  trap 'popd' 0
+
+  rsync --relative --quiet -vrae 'ssh -p 22' "." "${TARGET_DIR}" &
+
+  inotifywait -mr --timefmt '%FT%TZ' --format '%T %w %f %e' \
+    -e close_write -e create -e delete "${SOURCE_DIR}" | \
+    while read -r time dir file event; do
+
+    # construct file path relative to current directory
+    FILE="${dir//$(pwd)\//}${file}"
+
+    if [[ "${event}" == *DELETE* ]];
+    then
+        out "I, [${time}] $(echo "${FILE}" | sha256sum) deleted and not synchronized"
+    else
+      rsync --progress --relative --quiet -vrae 'ssh -p 22'  "${FILE}" "${TARGET_DIR}" && \
+        out "I, [${time}] $(echo "${FILE}" | sha256sum) synchronized"
+    fi
+  done
+}
+
+main "$@"
