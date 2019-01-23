4db5936b73b8ffa13d25fe51852f856141b5ac4f
david-berichon <davidberich@gmail.com>
2019-01-23 07:42:44 +0100

    builder+doc

--- prototool/build/rules.mk
+++ prototool/build/rules.mk
@@ -2,39 +2,45 @@ GO                     := go
 GRPC_TOOLS_RUBY_PROTOC := grpc_tools_ruby_protoc
 PROTOTOOL              := prototool
 
+ifndef SPEC_NAME
+SPEC_NAME = proto
+endif
+
+ifndef VERSION
+VERSION = $(shell [ -f VERSION ] && cat VERSION || echo alpha)
+endif
+
 RB_GEN_FILES = \
 	$(MODULES:%=build/ruby/lib/%_pb.rb) \
 	$(MODULES:%=build/ruby/lib/%_services_pb.rb) \
-	build/ruby/proto.gemspec
+	build/ruby/$(SPEC_NAME).gemspec
 
 GO_GEN_FILES = \
 	$(MODULES:%=build/go/%.pb.go)
 
-ifndef VERSION
-VERSION = $(shell cat VERSION)
-endif
-
+ifndef RB_GEMSPEC
 define RB_GEMSPEC
 lib = File.expand_path('../lib', __FILE__)
 $$LOAD_PATH.unshift(lib) unless $$LOAD_PATH.include?(lib)
 
 Gem::Specification.new do |spec|
-	spec.name = 'jobteaser-proto'
+	spec.name = '$(SPEC_PREFIX_NAME)$(SPEC_NAME)'
 	spec.version = '$(VERSION)'
 	spec.authors = ['Jobteaser']
 	spec.email = ['dev@jobteaser.com']
 
-	spec.summary = 'proto files for ruby'
+	spec.summary = '$(SPEC_NAME) files for ruby'
 	spec.description = 'This gem contains generated protocol buffer files for ruby'
 
 	spec.require_paths = ['lib']
 end
 endef
+endif
 
 export RB_GEMSPEC
 
 # Force this one because it depends on variable VERSION
-build/ruby/proto.gemspec: .FORCE
+build/ruby/%.gemspec: .FORCE
 	@mkdir -p $(@D)
 	@echo "$$RB_GEMSPEC" > $@
 
